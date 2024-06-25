28428911986cefdad7d3b01838192403a3582a0b
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-06-25 09:51:00 -0400

    Switch mit copy to per-session

--- etl/src/etl/msft-challenge/prepwaveform/mit_shiftselect.sql
+++ etl/src/etl/msft-challenge/prepwaveform/mit_shiftselect.sql
@@ -1,26 +1,9 @@
-SELECT b.merge_id,
-       a.name,
-       CASE WHEN extension = 'RECORDS' THEN
-       concat(split_part(a.name, '/', 3), '_',
-               to_char(b.start_time + INTERVAL '1' DAY * COALESCE(C.days, 0), 'YYYYmmDDHHmiSS'),
-              '_RECORDS')
-       WHEN (array_length((string_to_array(filename, '_')), 1) > 1) THEN
-       concat(split_part(a.name, '/', 3), '_',
-               to_char(b.start_time + INTERVAL '1' DAY * COALESCE(C.days, 0), 'YYYYmmDDHHmiSS'),
-              '_',
-              array_to_string((string_to_array(filename, '_'))[2:], '_'))
-       ELSE
-        concat(split_part(split_part(a.name, '/', 4), '.', 1), '_',
-              to_char(b.start_time + INTERVAL '1' DAY * COALESCE(C.days, 0), 'YYYYmmDDHHmiSS'),
-              '_main.',
-              split_part(split_part(a.name, '/', 4), '.', 2))
-        END AS modified_name
-FROM public.mit_allfiles a
-      INNER JOIN public.mit_wave_tmp b
-          ON a.person::bigint = b.merge_id
+SELECT a.record_id,
+       concat(a.record_id, '_',
+              to_char(a.start_time + INTERVAL '1' DAY * COALESCE(c.days, 0), 'YYYYmmDDHHmiSS')
+       )
+       AS modified_name
+FROM public.mit_wave_tmp a
 	  LEFT JOIN persist.date_shift C
-          ON b.merge_id = C.person_id
-WHERE extension IS NOT NULL
-    AND extension <> 'csv'
-ORDER BY random()
-LIMIT 100;
+          ON a.merge_id = C.person_id
+;

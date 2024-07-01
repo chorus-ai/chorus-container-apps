a121989889a2797a7ad6b9584c52f4237e5d4d94
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-07-01 16:50:26 -0400

    Update dynamic proxy_pass

--- www-challenge/http.js
+++ www-challenge/http.js
@@ -53,30 +53,13 @@ function whoami(r) {
     r.return(200, user);
 }
 
-function redirect_to_default_lab(r) {
-    let backend = r.variables.chorus_lab_backend_template,
-        user = r.variables.authenticated_user,
-        lab = backend.includes("*") ?
-            user.toLowerCase().replace(/@.*/, "").replace(/[^0-9a-z]+/g, "") :
-            "default",
-        uri = `/lab/${lab}/`;
-    if (has_access(user, uri)) {
-        r.return(303, uri);
-    }
-    else {
-        r.return(403);
-    }
+function get_challenge_backend(r) {
+    let user = r.variables.authenticated_user;
+    let lab = user.toLowerCase().replace(/@.*/, "").replace(/[^0-9a-z]+/g, "").substring(0,13);
+    let template = r.variables.chorus_lab_backend_template;
+    let end_uri =  template.replaceAll("*", lab).concat("/");
+    return end_uri;
 }
 
-function get_chorus_lab_backend(r) {
-    let template = r.variables.chorus_lab_backend_template,
-        uri = r.variables.uri,
-        lab = uri.match(/^\/lab\/([^/]+)/)[1] ?? "undefined";
-    lab = lab.toLowerCase().replace(/@.*/, "").replace(/[^0-9a-z]+/g, "");
-    if (template.endsWith("/")) {
-        template = template.slice(0, -1);
-    }
-    return template.replaceAll("*", lab);
-}
 
-export default { authorize, whoami, redirect_to_default_lab, get_chorus_lab_backend };
+export default { authorize, whoami, get_challenge_backend };

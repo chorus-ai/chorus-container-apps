93f3b27f8629d1969509b7af6515f537770ae1e3
Jared Houghtaling <67749079+jshoughtaling@users.noreply.github.com>
2024-11-04 14:12:46 -0500

    Add buffer refresh, logging

--- dash/read_blob_chorus.py
+++ dash/read_blob_chorus.py
@@ -12,7 +12,10 @@ for site_name in blob_list:
         site_cc = blob_svc.get_container_client(site_name)
         info = site_cc.list_blobs()
         cnt = 0
+        cnt2 = 0
+        cnt3 = 0
         data_tmp = pd.DataFrame()
+        print(f"Iterating over blob list for {site_name}")
         for dat in info:
                 for key in dat.keys():
                         value = dat[key]
@@ -20,7 +23,17 @@ for site_name in blob_list:
                         if key in column_list:
                                 data_tmp.at[cnt, key] = value
                 cnt = cnt + 1
-
+                if (cnt == 1000):
+                    cnt2 += cnt
+                    cnt3 += cnt
+                    print(f'{cnt2} Blobs Parsed...')
+                    cnt = 0
+                if (cnt3 == 100000):
+                    print("Refreshing dataframe buffer...")
+                    data_tmp['extension'] = data_tmp['name'].str.split(pat=".").str[-1]
+                    data_all = pd.concat([data_all, data_tmp[data_tmp['size']>0]])
+                    data_tmp = pd.DataFrame()
+                    cnt3 = 0
         data_tmp['extension'] = data_tmp['name'].str.split(pat=".").str[-1]
         data_all = pd.concat([data_all, data_tmp[data_tmp['size']>0]])
         print(f"Finished with {site_name}")

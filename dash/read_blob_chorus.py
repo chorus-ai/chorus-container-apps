f8a6078919c80aef456ca65c3ec62f8f8bd729d9
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-12-28 16:12:59 -0500

    Refactor dash approach

--- dash/read_blob_chorus.py
+++ dash/read_blob_chorus.py
@@ -1,41 +1,60 @@
 import os, uuid, sys, json
-import pandas as pd
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import subprocess
 connection_string = os.environ['PILOT_CONN_STR']
 blob_svc = BlobServiceClient.from_connection_string(conn_str=connection_string)
 blob_list = ["columbia","duke", "emory", "mayo", "mgh", "mit", "nationwide", "pitts", "seattle", "tuft", "ucla", "ucsf", "uflorida", "uva"]
 #blob_list = ["duke"]
 column_list = ["name", "container", "size", "last_modified", "creation_time"]
-data_all = pd.DataFrame(columns=column_list)
 cnt = 0
+TMP_CSV="/tmp/to_send.csv"
+
+def subprocess_run(command, input=None, cwd=None, env=None, check=True):
+    if isinstance(input, str):
+        input = input.encode('utf-8')
+    return subprocess.run(command, input=input, cwd=cwd, env=env, check=check)
+
+
+def psql_send_csv (csv_name, table_name="all_metadata"):
+    tmp_file = f'/tmp/load_{csv_name}.sh'
+    tmp_shell = f"""
+        tail -q -n +2 {csv_name} | 
+        psql -d ohdsi -h {PGHOST} -U postgres -p 5432 
+        -c \"\copy public.{table_name} FROM STDIN CSV DELIMITER E','\"
+        && rm -rf {csv_name}
+        """
+    with open(tmp_file, 'w') as t:
+        t.write("#!/bin/bash\n")
+        t.write(tmp_shell)
+    subprocess_run(['chmod', '+x', tmp_file], check=True)
+    subprocess_run([tmp_file], check=True)
 
 for site_name in blob_list:
         site_cc = blob_svc.get_container_client(site_name)
         info = site_cc.list_blobs()
         cnt2 = 0
         cnt3 = 0
-        data_tmp = pd.DataFrame()
         print(f"Iterating over blob list for {site_name}")
-        for dat in info:
-                for key in dat.keys():
-                        value = dat[key]
-                        #print(f"key: {key}, value: {value}")
+        with open(TMP_CSV, 'w') as t:
+            for dat in info:
+                if cnt3 == 0:
+                    t.write("id")
+                    for key in dat.keys():
                         if key in column_list:
-                                data_tmp.at[cnt, key] = value
+                            t.write(f",{key}")
+                    t.write("\n")
+                t.write(f"{cnt}")
+                for key in dat.keys():
+                    if key in column_list:
+                        t.write(f",{dat[key]}")
                 cnt = cnt + 1
                 cnt2 = cnt2 + 1
-                if (cnt2 == 1000):
-                    cnt3 += cnt2
-                    print(f'{cnt} Blobs Parsed...')
+                cnt3 = cnt3 + 1
+                t.write("\n")
+                if cnt2 == 100000:
+                    print(f'{cnt} Total Blobs Parsed...')
                     cnt2 = 0
-                if (cnt3 == 100000):
-                    print("Refreshing dataframe buffer...")
-                    data_tmp['extension'] = data_tmp['name'].str.split(pat=".").str[-1]
-                    data_all = pd.concat([data_all, data_tmp[data_tmp['size']>0]], ignore_index=True)
-                    data_tmp = pd.DataFrame()
-                    cnt3 = 0
-        data_tmp['extension'] = data_tmp['name'].str.split(pat=".").str[-1]
-        data_all = pd.concat([data_all, data_tmp[data_tmp['size']>0]], ignore_index=True)
+        #psql_send_csv(TMP_CSV, all_metadata)
         print(f"Finished with {site_name}")
 
-data_all.to_csv("/pilot_meta/data/all_metadata.csv")
+

fd43d4451c4710988a381a3476976ac4047de1b4
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-12-28 19:17:44 -0500

    Catch NOTE tables that fall into OMOP dirs

--- dash/prep_for_export.sql
+++ dash/prep_for_export.sql
@@ -13,10 +13,10 @@ SELECT     name,
            date_part('month', creation_time::timestamp) AS month_created,
            FLOOR(((date_part('month', creation_time::timestamp)-1) / 3) + 1) AS quarter_created,
            CASE WHEN char_length(split_part(name,'.', -1)) < 10 THEN split_part(name,'.', -1) END AS extension,
-           CASE WHEN lower(name) LIKE '%omop%' OR lower(name) LIKE 'omop%' OR split_part(name,'.', -1) = 'csv' OR split_part(name,'.', -1) = 'xlsx' THEN 'OMOP'
+           CASE WHEN lower(name) LIKE '%note%' OR lower(name) LIKE 'note%' THEN 'NOTE'
+                WHEN lower(name) LIKE '%omop%' OR lower(name) LIKE 'omop%' OR split_part(name,'.', -1) = 'csv' OR split_part(name,'.', -1) = 'xlsx' THEN 'OMOP'
                 WHEN lower(name) LIKE '%wave%' OR lower(name) LIKE 'wave%' THEN 'WAVE'
                 WHEN lower(name) LIKE '%image%' OR lower(name) LIKE 'image%' THEN 'IMAGE'
-                WHEN lower(name) LIKE '%note%' OR lower(name) LIKE 'note%' THEN 'NOTE'
            END AS mode,
            CURRENT_TIMESTAMP AS loaded_at
     FROM public.all_metadata WHERE size::float > 0

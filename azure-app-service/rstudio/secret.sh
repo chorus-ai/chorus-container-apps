0b03f04e3d118af7e5a29b6750cdd6912197c457
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-01-28 17:01:28 -0500

    Add rstudio pre-built with users

---
+++ azure-app-service/rstudio/secret.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -A array;
+eval ${1}
+
+for i in "${!array[@]}"; do
+    username=$i;
+    password=${array[$i]};
+    adduser --gecos "" --disabled-password $username;
+    chpasswd <<<"$username:$password";
+done

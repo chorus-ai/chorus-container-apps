c076fc394213b66c8fba0ea7901ad2f1bb0715e9
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-07-11 12:30:00 +0200

    Add engineer portal

---
+++ www-engineer/atlas/config-local.js
@@ -0,0 +1,12 @@
+define([], function () {
+    var configLocal = {};
+
+    configLocal.api = {
+        name: 'CHoRUS Engineer',
+        url: '/webapi/'
+    };
+
+    configLocal.userAuthenticationEnabled = false;
+
+    return configLocal;
+});

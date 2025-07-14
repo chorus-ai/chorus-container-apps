c95cddc487182da9469941b573e3d5517745b600
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-07-14 22:22:04 +0200

    Add SCCM gcp deployment

---
+++ www-sccm-gcp/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+set -o pipefail
+
+if [[ $# -eq 0 ]]; then
+    ENTRY_COMMAND="web"
+else
+    ENTRY_COMMAND="$1"
+fi
+
+echo "Launching Entrypoint ->${ENTRY_COMMAND}"
+
+if [[ $ENTRY_COMMAND == "" ]]; then
+ exit 0
+elif [[ $ENTRY_COMMAND == "web" ]]; then
+  if [ "${HTTP_AUTH_PASSWORD}" != "" ] && [ "${HTTP_AUTH_LOGIN}" != "" ]; then
+    sed -i "s/#auth_basic/auth_basic/g;" /etc/nginx/conf.d/default.conf
+    rm -rf /etc/nginx/.htpasswd
+    echo -n $HTTP_AUTH_LOGIN:$(openssl passwd -apr1 $HTTP_AUTH_PASSWORD) >> /etc/nginx/.htpasswd
+    echo "Basic auth is on for user ${HTTP_AUTH_LOGIN}..."
+  fi
+  if [[ "${OAUTH2_FORCE_HTTPS:-}" ]] && [[ "${OAUTH2_FORCE_HTTPS}" == "false" ]]; then
+      FORCE_HTTPS="-cookie-secure=false -force-https=false"
+  else
+      FORCE_HTTPS="-cookie-secure=true -force-https=true"
+  fi
+  service nginx start
+  # https://oauth2-proxy.github.io/oauth2-proxy/configuration
+  oauth2_proxy -client-id="${OAUTH2_CLIENT_ID}" \
+              -client-secret="${OAUTH2_CLIENT_SECRET}" \
+              -provider="${OAUTH2_PROVIDER}" \
+              -cookie-secret="${OAUTH2_COOKIE_SECRET}" \
+              ${FORCE_HTTPS} \
+              -set-xauthrequest=true \
+              -email-domain="${OAUTH2_EMAIL_DOMAIN}" -upstream file:///dev/null &
+
+else
+  # Non blocking entrypoint starting bash
+  exec "$@"
+fi
---
+++ www-sccm-gcp/http.js
@@ -0,0 +1,56 @@
+function get_roles_by_user(user) {
+    let roles = [user],
+        k = 0;
+    while (k < roles.length) {
+        let other_roles = access.roles[roles[k]] ?? [];
+        for (let i = 0; i < other_roles.length; i ++) {
+            const other_role = other_roles[i];
+            if (!roles.includes(other_role)) {
+                roles.push(other_role);
+            }
+        }
+        k ++;
+    }
+    if (!roles.includes("*")) {
+        roles.push("*");
+    }
+    return roles;
+}
+
+function get_roles_by_uri(uri) {
+    let path = "",
+        roles = [];
+    for (const other_path in access.paths) {
+        if (other_path.endsWith("/")) {
+            other_path = other_path.slice(0, -1);
+        }
+        if (uri == other_path || uri.startsWith(`${other_path}/`)) {
+            if (other_path.length >= path.length) {
+                path = other_path;
+                roles = [].concat(access.paths[path]);
+            }
+        }
+    }
+    return roles;
+}
+
+function has_access(user, uri) {
+    let user_roles = get_roles_by_user(user),
+        uri_roles = get_roles_by_uri(uri);
+    return user_roles.some(r => uri_roles.includes(r));
+}
+
+function authorize(r) {
+    let user = r.variables.authenticated_user,
+        uri = r.variables.protected_uri;
+    r.return(has_access(user, uri) ? 200 : 403);
+}
+
+function whoami(r) {
+    let user = r.variables.authenticated_user;
+    r.warn(`/whoami: ${user}`);
+    r.headersOut["Content-Type"] = "text/plain"
+    r.return(200, user);
+}
+
+export default { authorize, whoami };

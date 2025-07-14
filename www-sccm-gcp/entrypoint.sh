39147078f727a5b5e8138bc489c76776927ca6c3
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-07-15 00:14:49 +0200

    Fix argument format in entrypoint

--- www-sccm-gcp/entrypoint.sh
+++ www-sccm-gcp/entrypoint.sh
@@ -16,21 +16,17 @@ echo "Launching Entrypoint ->${ENTRY_COMMAND}"
 if [[ $ENTRY_COMMAND == "" ]]; then
  exit 0
 elif [[ $ENTRY_COMMAND == "web" ]]; then
-  if [[ "${OAUTH2_FORCE_HTTPS:-}" ]] && [[ "${OAUTH2_FORCE_HTTPS}" == "false" ]]; then
-      FORCE_HTTPS="-cookie-secure=false -force-https=false"
-  else
-      FORCE_HTTPS="-cookie-secure=true -force-https=true"
-  fi
   service nginx start
   # https://oauth2-proxy.github.io/oauth2-proxy/configuration
-  /usr/bin/oauth2-proxy -client-id="${OAUTH2_CLIENT_ID}" \
-              -client-secret="${OAUTH2_CLIENT_SECRET}" \
-              -provider="${OAUTH2_PROVIDER}" \
-              -cookie-secret="${OAUTH2_COOKIE_SECRET}" \
-              ${FORCE_HTTPS} \
-              -set-xauthrequest=true \
-              -redirect-url=${OAUTH2_REDIRECT_URL} \
-              -email-domain="${OAUTH2_EMAIL_DOMAIN}" -upstream file:///dev/null &
+  /usr/bin/oauth2-proxy --client-id="${OAUTH2_CLIENT_ID}" \
+             --client-secret="${OAUTH2_CLIENT_SECRET}" \
+             --provider="${OAUTH2_PROVIDER}" \
+             --cookie-secret="${OAUTH2_COOKIE_SECRET}" \
+             --cookie-secure=false \
+             --force-https=false \
+             --set-xauthrequest=true \
+             --redirect-url="${OAUTH2_REDIRECT_URL}" \
+             --email-domain="${OAUTH2_EMAIL_DOMAIN}" --upstream file:///dev/null &
 
 else
   # Non blocking entrypoint starting bash

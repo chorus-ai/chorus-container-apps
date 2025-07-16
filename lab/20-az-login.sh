fcadd59e41a49a986be1056030086c19b20289ca
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-07-16 14:27:09 +0200

    Add debug to sort out changes to cli login

--- lab/20-az-login.sh
+++ lab/20-az-login.sh
@@ -1,3 +1,3 @@
 if [ -n "$AZURE_CLIENT_ID" ]; then
-    az login --identity --client-id $AZURE_CLIENT_ID --allow-no-subscriptions
+    az login --identity --client-id $AZURE_CLIENT_ID --allow-no-subscriptions --debug
 fi

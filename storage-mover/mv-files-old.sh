a77b038e02c68a69c0aa6d4465f2ba72c8f8989b
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-07-17 11:09:46 +0200

    Update storage mover script

---
+++ storage-mover/mv-files-old.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Copying files from landing storage to midpoint FS"
+
+L2M=$(azcopy cp --recursive --check-md5=NoCheck "${LANDING_KEY}" "/midpoint")
+
+if [[ $L2M == *"Final Job Status: Completed"* ]] && [[ $L2M != *"Final Job Status: CompletedWithErrors"* ]]; then
+  echo "Copy to midpoint completed successfully! Removing files in landing storage..."
+  RML=$(azcopy remove --recursive --exclude-pattern="*.db;*.locked" "${LANDING_KEY}")
+  if [[ $RML == *"Final Job Status: Completed"* ]] && [[ $RML != *"Final Job Status: CompletedWithErrors"* ]]; then
+    echo "Landing file(s) removed! Launching copy from midpoint to secure storage..."
+    else
+      echo "$RML"
+      exit 1
+  fi
+  else
+    echo "$L2M"
+    exit 1
+fi
+
+M2P=$(azcopy cp --recursive --check-md5=NoCheck "/midpoint/*" "${PILOT_KEY}")
+
+if [[ $M2P == *"Final Job Status: Completed"* ]] && [[ $M2P != *"Final Job Status: CompletedWithErrors"* ]]; then
+  echo "Copy from midpoint completed successfully! Removing files in midpoint..."
+  rm -rf /midpoint/*
+  else
+    echo "$M2P"
+    exit 1
+fi
+
+echo "Storage move job complete! Printing logs and Exiting..."
